2b8b957508996216e3d667311dd1c34d40205a13
melo-maniac-29 <allenbobby2003@gmail.com>
2025-02-22 11:21:16 +0530

    added button-darkmode from sahdcn

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>Hello world</h2>
+      <Button variant="outline">Click me</Button>
     </div>
   );
 }
---
+++ app/provider.jsx
@@ -0,0 +1,9 @@
+import React from 'react';  //for dark theme providing after installing npm dark theme mode do this
+
+function provider(){
+    return (
+        <div>Provider</div>
+    )
+}
+
+export default provider;
