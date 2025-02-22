bce39872d80c74dc5959e820dc9b6d5efc7ba61b
melo-maniac-29 <allenbobby2003@gmail.com>
2025-02-22 11:34:12 +0530

    project setup complete

--- app/provider.jsx
+++ app/provider.jsx
@@ -1,9 +1,19 @@
+"use client" //client side rendering
 import React from 'react';  //for dark theme providing after installing npm dark theme mode do this
+import { ThemeProvider as NextThemesProvider } from "next-themes"
 
-function provider(){
+function Provider({children}) {
     return (
-        <div>Provider</div>
+        <div>
+            <NextThemesProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange>
+            {children}
+            </NextThemesProvider>
+        </div>
     )
 }
 
-export default provider;
+export default Provider;
