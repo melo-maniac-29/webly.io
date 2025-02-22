8c10419aa5bdbbde12c2de53d0098c86db115a50
melo-maniac-29 <allenbobby2003@gmail.com>
2025-02-23 02:42:37 +0530

    dialog box sigin setup complete/auth if signout dialog

--- components/custom/SignInDialog.jsx
+++ components/custom/SignInDialog.jsx
@@ -8,17 +8,22 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import Lookup from "@/data/Lookup";
+import { Button } from "../ui/button";
 
-function SignInDialog({ openDialog,closeDialog }) {
+function SignInDialog({ openDialog, closeDialog }) {
   return (
     <div>
       <Dialog open={openDialog} onOpenChange={closeDialog}>
         <DialogContent>
           <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogTitle className="font-bold text-2xl text-center text-white gap-3">{Lookup.SIGNIN_HEADING}</DialogTitle>
             <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
+              <div className="flex flex-col items-center justify-center gap-4">
+                <p className="mt-3 text-center">{Lookup.SIGNIN_SUBHEADING}</p>
+                <Button className="bg-blue-500 text-white hover:bg-blue-400 mt-3">Sign In With Google</Button>
+                <p className="text-xs mt-2">{Lookup.SIGNIn_AGREEMENT_TEXT}</p>
+              </div>
             </DialogDescription>
           </DialogHeader>
         </DialogContent>
