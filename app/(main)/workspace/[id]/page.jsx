71a16399e215af336a4a385b401bf841e4f5df5f
melo-maniac-29 <allenbobby2003@gmail.com>
2025-02-24 19:35:11 +0530

    setup of img-in chatview and message prompt fetch and display-success

--- app/(main)/workspace/[id]/page.jsx
+++ app/(main)/workspace/[id]/page.jsx
@@ -8,7 +8,7 @@ import React from 'react'
 function Workspace() {
   return (
     <div className='p-10'>
-        <div className='grid grid-cols-1 md:grid-cols-3'>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-10'>
 
           <ChatView />
 
--- components/custom/ChatView.jsx
+++ components/custom/ChatView.jsx
@@ -1,14 +1,22 @@
 "use client";
+import { MessagesContext } from '@/context/MessagesContext';
+import { UserDetailContext } from '@/context/UserDetailContext';
 import { api } from '@/convex/_generated/api';
+import Colors from '@/data/Colors';
 import { useConvex } from 'convex/react';
 import { useParams } from 'next/navigation'
-import React, { use, useEffect } from 'react'
+import React, { use, useContext, useEffect, useState } from 'react'
+import Image from 'next/image';
 
 function ChatView() {  //used to fetch workspace data by workspacw id
 
   const {id}=useParams();  //useParams is used to get the dynamic id from the url
 
   const convex=useConvex();  //useConvex is used to get the convex object
+
+  const { messages, setMessages } = useContext(MessagesContext); //getting messages from the context
+
+  const {userDetail,setUserDetail}=useContext(UserDetailContext);  //getting user details from the context
 
   useEffect(() => {  //useEffect is used to run the function when the component is mounted
     id && GetWorkspaceData();  //function to get the workspace data
@@ -21,12 +29,31 @@ function ChatView() {  //used to fetch workspace data by workspacw id
         workspaceId:id
       }
     );
+    setMessages(result.messages);  //set the messages in the context
+
     console.log(result);
   }
 
   return (
     <div>
-      ChatView
+          <div>
+            {messages?.map((msg,index)=>(  //mapping the messages   img displaying the user image
+              <div key={index} 
+              className='p-3 rounded-lg mb-2 flex gap-2 items-start'
+              style={{
+                backgroundColor:Colors.CHAT_BACKGROUND
+              }}>
+
+                {msg?.role=='user' && 
+                <Image src={ userDetail?.picture}  alt='userImage'
+                width={35} height={35} className='rounded-full'  /> }  
+
+                <h2>{msg.content}</h2>    
+
+              </div>
+            ))}
+          </div>
+          {/* Input section */}
     </div>
   )
 }
