995be8c58ec078ee62e4c3463a68bde3b4e29dba
melo-maniac-29 <allenbobby2003@gmail.com>
2025-02-25 18:06:16 +0530

    code-view session almost complete

--- components/custom/CodeView.jsx
+++ components/custom/CodeView.jsx
@@ -1,18 +1,28 @@
+"use client"; //client side rendering
+
 import React from "react";
 import {
   SandpackProvider,
   SandpackLayout,
   SandpackCodeEditor,
   SandpackPreview,
+  SandpackFileExplorer
 } from "@codesandbox/sandpack-react";
 
 function CodeView() {
   return (
     <div>
-      <SandpackProvider template="react">
+      <div>
+        <div>
+          <h2>code</h2>
+          <h2>preview</h2>
+        </div>
+      </div>
+      <SandpackProvider template="react" theme={"dark"}>
         <SandpackLayout>
-          <SandpackCodeEditor />
-          <SandpackPreview />
+          <SandpackFileExplorer  style={{height:'80vh'}} />
+          <SandpackCodeEditor  style={{height:'80vh'}} />
+          <SandpackPreview style={{height:'80vh'}} />
         </SandpackLayout>
       </SandpackProvider>
     </div>
