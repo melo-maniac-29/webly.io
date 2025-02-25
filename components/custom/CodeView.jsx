393ab8d730f0c93706f20410c15fbbbb758e5bc0
melo-maniac-29 <allenbobby2003@gmail.com>
2025-02-25 21:04:30 +0530

    got response code-in network-console

--- components/custom/CodeView.jsx
+++ components/custom/CodeView.jsx
@@ -1,6 +1,6 @@
 "use client"; //client side rendering
 
-import React, { useState } from "react"; // Add useState import
+import React, { useState, useContext, useEffect } from "react"; // Add useContext and useEffect imports
 import {
   SandpackProvider,
   SandpackLayout,
@@ -9,11 +9,41 @@ import {
   SandpackFileExplorer,
 } from "@codesandbox/sandpack-react";
 import Lookup from "@/data/Lookup";
+import axios from "axios";
+import { MessagesContext } from "@/context/MessagesContext";
+import Prompt from "@/data/Prompt";
 
 function CodeView() {
   const [activeTab, setActiveTab] = useState("code"); // Fixed useState hook
 
   const [files, setFiles] = useState(Lookup?.DEFAULT_FILE); // Fixed useState hook
+
+  const {messages, setMessages} = useContext(MessagesContext); //Fixed useContext hook
+
+  useEffect(() => {
+    if (messages?.length > 0) {
+      /*if messages are present then get the AI response*/
+
+      const role = messages[messages?.length - 1].role;
+
+      if (role == "user") {
+        GenerateAiCode(); //chat response succesfully-created
+      }
+    }
+  }, [messages]);
+
+  const GenerateAiCode = async () => {
+    const PROMPT =
+      messages[messages?.length - 1]?.content + " " + Prompt.CODE_GEN_PROMPT;
+    const result = await axios.post("/api/gen-ai-code", {
+      prompt: PROMPT,
+    });
+    console.log(result.data);
+    const aiResp = result.data;
+
+    const mergedFiles = { ...Lookup.DEFAULT_FILE, ...aiResp?.files };
+    setFiles(mergedFiles);
+  };
 
   return (
     <div>
